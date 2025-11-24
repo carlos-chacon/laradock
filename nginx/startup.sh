617a82b672cf027e1ec7b5bfccb7523ef60f5708
Carlos Chacón <ccarch81@gmail.com>
2025-11-24 07:56:31 -0500

    feat(nginx): Add SSL configuration and update certificate generation logic

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -17,7 +17,11 @@ fi
 if [ ! -f /etc/nginx/ssl/apipdf-nuwwe.pp.crt ]; then
     openssl genrsa -out "/etc/nginx/ssl/apipdf-nuwwe.pp.key" 2048
     openssl req -new -key "/etc/nginx/ssl/apipdf-nuwwe.pp.key" -out "/etc/nginx/ssl/apipdf-nuwwe.pp.csr" -subj "/CN=apipdf-nuwwe.pp/O=apipdf-nuwwe.pp/C=UK"
-    openssl x509 -req -days 365 -in "/etc/nginx/ssl/apipdf-nuwwe.pp.csr" -signkey "/etc/nginx/ssl/apipdf-nuwwe.pp.key" -out "/etc/nginx/ssl/apipdf-nuwwe.pp.crt"
+    if [ -f /etc/nginx/config_ssl/nuwwe_v3.ext ]; then
+        openssl x509 -req -days 365 -in "/etc/nginx/ssl/apipdf-nuwwe.pp.csr" -signkey "/etc/nginx/ssl/apipdf-nuwwe.pp.key" -out "/etc/nginx/ssl/apipdf-nuwwe.pp.crt" -extfile "/etc/nginx/config_ssl/nuwwe_v3.ext"
+    else
+        openssl x509 -req -days 365 -in "/etc/nginx/ssl/apipdf-nuwwe.pp.csr" -signkey "/etc/nginx/ssl/apipdf-nuwwe.pp.key" -out "/etc/nginx/ssl/apipdf-nuwwe.pp.crt" -extfile <(printf "authorityKeyIdentifier=keyid,issuer\nbasicConstraints=CA:FALSE\nkeyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment\nsubjectAltName = @alt_names\n\n[alt_names]\nDNS.1 = apipdf-nuwwe.pp\nDNS.2 = *.apipdf-nuwwe.pp\n")
+    fi
     chmod 644 /etc/nginx/ssl/apipdf-nuwwe.pp.key
 fi
 
